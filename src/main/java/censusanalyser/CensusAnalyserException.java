601f3f3dfd6879147b1af8d0c536d17aad68f57f
Pallavi-Srivastava <pallavisrivastava858@gmail.com>
2020-11-15 07:17:22 +0530

    Added:Code for finding Data Info in Sorted Order On Populous StateWise

---
+++ src/main/java/censusanalyser/CensusAnalyserException.java
@@ -0,0 +1,26 @@
+package censusanalyser;
+
+public class CensusAnalyserException extends Exception {
+
+	enum ExceptionType {
+		CENSUS_FILE_PROBLEM, WRONG_FILE_TYPE, NO_SUCH_FILE, NO_SUCH_FIELD, NO_CENSUS_DATA
+
+	}
+
+	public ExceptionType type;
+
+	public CensusAnalyserException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+
+	public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+		super(message, cause);
+		this.type = type;
+	}
+
+	public CensusAnalyserException(String message, String name) {
+		super(message);
+		this.type = ExceptionType.valueOf(name);
+	}
+}
